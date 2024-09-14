fd8298a3b3257d39cb5d155d57037b52c2292c43
Mradul <mradul9936@gmail.com>
2024-09-15 03:01:58 +0530

    changes in token generations

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -5,12 +5,13 @@ const generateTokenAndSetCookie=(userId,res)=>{
   const token=jwt.sign({userId}, process.env.JWTSECRET
     ,{expiresIn:'30d'});
     console.log(token)
-    res.cookie("ETest",token,{
+    
+    res.cookie("ETest", token, {
       maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
-      httpOnly: false,
-      sameSite: 'strict',
-      secure: process.env.NODE_ENV === 'production', // Only set to true in production
-    })
+      httpOnly: true, // Prevent JavaScript access to cookies
+      secure: process.env.NODE_ENV === 'production', // Use 'secure' in production only
+      sameSite: 'None', // Allows cross-origin cookies (important for CORS)
+    });
         console.log("cookies set successfully")
 }
 
