23fa406d02824b35d2c5b317276ed6fc05b6d898
Mradul Vishwakarma <77965652+MRADUL9936@users.noreply.github.com>
2024-10-07 04:54:19 +0530

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ import './jobs/cronJob.jobs.js'
 
 dotenv.config()
 const app=express()
-const whitelist = ['http://localhost:5173','https://etest.mradulvishwakarma.com/'];
+const whitelist = ['http://localhost:5173','https://etest.mradulvishwakarma.com'];
 
 // Configure the CORS middleware
 app.use(cors({
